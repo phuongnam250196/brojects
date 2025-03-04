ae1ef336d02cb8293079fc6b5e4b946226fa367f
NamTP <namtp@regit-technology.com>
2025-03-04 20:53:01 +0700

    First up

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  $("#toggle").click(function () {
+    $(this).toggleClass("open");
+    $("#overlay").toggleClass("open");
+    $("#header-fix").toggleClass("open");
+
+    if ($("#overlay li").hasClass("open")) {
+      $("#overlay li").removeClass("open");
+    }
+  });
+  $("#overlay li a").on("click", function (e) {
+    const overlayItems = $("#overlay li");
+    const parentItem = $(this).parent();
+    const overlay = $("#overlay");
+
+    if (e.target.href.includes("javascript:void")) {
+      if (!parentItem.hasClass("open")) {
+        overlayItems.removeClass("open");
+      }
+      parentItem.toggleClass("open");
+      return;
+    }
+
+    overlayItems.removeClass("open");
+    overlay.toggleClass("open");
+  });
+
+  $("#overlay .overlay-bg").on("click", function (e) {
+    $("#overlay li").removeClass("open");
+    $("#overlay").removeClass("open");
+  })
+
+  $(".owl-banner").owlCarousel({
+    loop: true,
+    center: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    // autoWidth:true,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+    // autoplaySpeed: 1500,
+    items: 1,
+  });
+
+  // $(window).on("load scroll", function () {
+  //   //現時点のスクロールの高さ取得
+  //   const scrollPosition = $(window).scrollTop();
+  //   if (scrollPosition > 109) {
+  //     $("#header").css({
+  //       background: "var(--c-gray-2)",
+  //     });
+  //   } else {
+  //     $("#header").css({ background: "unset" });
+  //   }
+  // });
+});
